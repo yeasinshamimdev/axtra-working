1fa05f59f70e58b373859de00c743d52e5deaad9
yeasin_shamim <yeasinshamim7@gmail.com>
2023-01-08 10:24:05 +0600

    package installed

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -10,6 +10,10 @@ class MyDocument extends Document {
         return (
             <Html>
                 <Head>
+                    <link
+                        rel="stylesheet"
+                        href="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css"
+                    />
                     {/* Google Fonts  */}
                     <link rel="preconnect" href="https://fonts.googleapis.com" />
                     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
