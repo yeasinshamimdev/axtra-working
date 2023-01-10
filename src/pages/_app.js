a0a6e8fc7804889927d4b3526f845aa804dfcb26
yeasin_shamim <yeasinshamim7@gmail.com>
2023-01-10 11:42:32 +0600

    edited

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,15 +6,17 @@ import '../../public/assets/css/master.css'
 import '../../public/assets/css/meanmenu.min.css'
 import '../../public/assets/css/swiper-bundle.min.css'
 import UseScript from '../../scripts/useScript';
-import NoSSR from 'react-no-ssr';
-import Placeholder from '../components/common/placeholder.jsx';
 
 function MyApp({ Component, pageProps }) {
 
-  return <NoSSR onSSR={<Placeholder />}>
+  return <>
     <Component {...pageProps} />
     <UseScript src={['assets/js/jquery-3.6.0.min.js', 'assets/js/bootstrap.bundle.min.js', 'https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.js', 'assets/js/counter.js', 'https://cdnjs.cloudflare.com/ajax/libs/gsap/3.11.4/gsap.min.js', 'assets/js/ScrollTrigger.min.js', 'assets/js/ScrollToPlugin.min.js', 'assets/js/ScrollSmoother.min.js', 'assets/js/SplitText.min.js', 'assets/js/chroma.min.js', 'assets/js/jquery.meanmenu.min.js', 'assets/js/main.js']} />
-  </NoSSR>
+  </>
+}
+
+export const config = {
+  disableSsr: true
 }
 
 export default MyApp
